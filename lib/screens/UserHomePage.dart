1e6edea6cbd0b5b89903032ae77d34472c9e1861
matt <matt.law@gmail.com>
2020-02-12 21:52:34 +0000

    Invoking the user profile endpoint for data

--- lib/screens/UserHomePage.dart
+++ lib/screens/UserHomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:requests/requests.dart';
 
 // User Profile Page
 // keeping it as stateful as we'll be passing in the auth token
@@ -10,8 +11,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  Map widgetData = {};
+
   @override
   Widget build(BuildContext context) {
+
+    widgetData = ModalRoute.of(context).settings.arguments;
+
+    String authToken = widgetData['authToken'];
+
+    print( 'auth token data is $authToken');
 
     return Scaffold(
       appBar: AppBar(
@@ -32,13 +41,58 @@ class _HomePageState extends State<HomePage> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print('Load the user profile');
+                  dynamic profileData = await _getProfileData(authToken);
+
+                  if(profileData != null) {
+                    // load the details page
+                  }
+                  else {
+                    // handle the error here with a nice message?
+                  }
+
+
                 }
             ),
           ],
         ),
       )
     );
+  }
 
+  // private method to get the profile data
+  Future<dynamic> _getProfileData(authToken) async {
+
+    // some debug to make sure the values are coming through ok
+    print('Getting profile data with token $authToken');
+
+    Response response;
+
+    // Setup the headers as a map
+    Map<String, String> requestHeaders = {};
+    requestHeaders['ClasslistToken'] = authToken;
+
+    try {
+
+      // invoke the API to load profile data
+      response = await Requests.get(
+          'https://api.classlist.com/_ah/api/parent/v1/70670680/get/myprofile',
+          headers: requestHeaders
+      );
+
+      response.raiseForStatus();
+
+      // get the response and debug it
+      dynamic json = response.json();
+
+      print('Profile data is\n $json');
+
+      return json;
+    }
+    catch(e)
+    {
+      print('Failed to log in successfully $e ${response.statusCode}');
+    }
+
+    return null;
   }
 }
