d479addda5b007dd14459aea2e8d9182c704dbc0
matt <matt.law@gmail.com>
2020-02-12 19:48:01 +0000

    Homepage widget

---
+++ lib/screens/HomePage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+//Moved HomePage widget to a separate file
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Home page screen',
+            ),
+
+          ],
+        ),
+      )
+    );
+  }
+}
