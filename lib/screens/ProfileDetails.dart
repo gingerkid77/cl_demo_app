164325c34eb23bba7a641919016ce8e04dc770ce
matt <matt.law@gmail.com>
2020-02-12 20:53:17 +0000

    Additional fields and avatar test

--- lib/screens/ProfileDetails.dart
+++ lib/screens/ProfileDetails.dart
@@ -30,6 +30,11 @@ class _ProfileDetailsPageState extends State<ProfileDetailsPage> {
           //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            
+            CircleAvatar(
+              backgroundImage: NetworkImage('https://storage.googleapis.com/class-list-api-europe/5776637916348416'),
+              radius: 50.0,
+            ),
             Text(
               'Name',
               style: TextStyle(
@@ -45,6 +50,38 @@ class _ProfileDetailsPageState extends State<ProfileDetailsPage> {
                 'Address',
                 style: TextStyle(
                   fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
+            Text(
+                'Address goes here'
+            ),
+            SizedBox(height: 15.0),
+            Text(
+                'Pupils',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
+            Text(
+                'List of pupils goes here'
+            ),
+            SizedBox(height: 15.0),
+            Text(
+                'Family Members',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
+            Text(
+                'Known family members'
+            ),SizedBox(height: 15.0),
+            Text(
+                'Invites',
+                style: TextStyle(
+                  fontSize: 15.0,
                   fontWeight: FontWeight.bold,
                 )
             ),
