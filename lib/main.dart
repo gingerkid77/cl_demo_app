370c6388bd49b29fafe5e52620ea33bbc5a022bb
matt <matt.law@gmail.com>
2020-02-13 00:09:40 +0000

    added loading page

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
+import 'package:cl_demo_app/screens/Loading.dart';
 import 'package:cl_demo_app/screens/ProfileDetails.dart';
 import 'package:cl_demo_app/screens/UserHomePage.dart';
 import 'package:flutter/material.dart';
 import 'screens/LoginPage.dart';
 
+// set up the routes and start the app
 void main() => runApp(MaterialApp(
-
     initialRoute: '/',
     routes: {
-      '/': (context) => LoginPage(),
+      '/': (context) => Loading(),
+      '/login': (context) => LoginPage(),
       '/home': (context) => HomePage(),
       '/profile': (context) => ProfileDetailsPage(),
     }
-
 ));
 
 
